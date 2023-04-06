85580fb15eb175db4f9dc436f930ea39c7011789
Isabelle <isabelleroet@gmail.com>
2023-04-05 17:51:29 -0700

    Third Commit

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "1f3758ac26276dd0a33df5937b7c2168"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "48a0c7cdbcef48a6adb6a9d446904bca"
